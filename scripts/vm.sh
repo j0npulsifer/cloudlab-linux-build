e8d4b88dc57f5b20bd8cd614b5fb8edc806482f4
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-07-29 21:35:46 -0400

    esx

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -15,27 +15,20 @@ add_apt_repo() {
   add-apt-repository "deb [arch=amd64] ${1}"
 }
 
-# datadog
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 382E94DE
-add_apt_repo "https://apt.datadoghq.com/ stable 6"
+add_gpg_keys "https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+add_apt_repo "http://apt.kubernetes.io kubernetes-xenial main"
 
 # docker
 add_gpg_keys "https://download.docker.com/linux/ubuntu/gpg"
 add_apt_repo "https://download.docker.com/linux/ubuntu ${RELEASE} stable"
-
-# falco
-add_gpg_keys "https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public"
-add_apt_repo "http://download.draios.com/stable/deb stable-\$(ARCH)/"
 
 # update the things
 apt-get -qqy update
 
 # install the things
 apt-get -qqy install \
-  datadog-agent \
   docker-ce \
-  falco \
   linux-headers-"$(uname -r)" \
-  unattended-upgrades
+  kubelet
 
 usermod -aG docker "${LINUX_USER}"
