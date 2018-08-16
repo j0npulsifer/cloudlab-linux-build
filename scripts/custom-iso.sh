65d32537f3bd29089190057c730b98131a7e3bec
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2018-08-16 19:33:00 -0400

    letsgo
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

--- scripts/custom-iso.sh
+++ scripts/custom-iso.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 set -eu
 
-# where we do the things
-WORKDIR="tmpdir"
-ISO_MOUNT_DIR="$(mkdir -p ${WORKDIR}/mount)"
-CDIMAGE_DIR="$(mkdir -p ${WORKDIR}/cdimage)"
+# run as root check
+! [ "$(id -u)" = "0" ] && { echo "Please run this script as root"; exit 1; }
+# macos check
+! [ "$(uname)" = "Darwin" ] && { echo "This script works on macOS only sry bout that"; exit 1; }
+# dependencies
+command -v gpg >/dev/null || { echo "Can not find gpg. Please run: brew install gpg"; exit 1; }
+command -v mkisofs >/dev/null || { echo "Can not find mkisofs. Please run: brew install cdrtools"; exit 1; }
+
+# where we build
+WORKDIR="build"
+ISO_MOUNT_DIR="mount"
+CD_IMAGE_DIR="cd-image"
+# create the dirs
+mkdir -p "${WORKDIR}" "${WORKDIR}/${ISO_MOUNT_DIR}" "${WORKDIR}/${CD_IMAGE_DIR}"
 
 #### CHANGE ME ####
-# this is the custom iso path
-CUSTOM_ISO_PATH="/tmp/fresh.iso"
+# this is where you want the custom iso to end up
+CUSTOM_ISO_PATH="fresh.iso"
 
 # ubuntu iso and checksum
 UBUNTU_VERSION="16.04"
@@ -21,6 +31,8 @@ ISO_URL="${BASE_URL}/ubuntu-${UBUNTU_VERSION}.${PATCH_VERSION}-server-amd64.iso"
 
 # clean up when we're done no matter wat
 cleanup() {
+    # only unmount if there is an error
+    [ $? -gt 0 ] && unmount_iso
     echo "Run this command to remove all the temp files:"
     echo /bin/rm -rv "${WORKDIR}"
 }
@@ -31,6 +43,7 @@ trap cleanup EXIT INT TERM
 download_iso() { 
     curl -OJL "${ISO_URL}"
 }
+
 download_checksums() { 
     curl -OJL "${BASE_URL}/SHA256SUMS"
     curl -OJL "${BASE_URL}/SHA256SUMS.gpg"
@@ -41,44 +54,34 @@ verify_files() {
     gpg --verify SHA256SUMS.gpg SHA256SUMS
 
     # probably a better way to do this
-    sha256sum -c SHA256SUMS 2>&1 | grep OK
+    shasum -a 256 -c SHA256SUMS 2>&1 | grep OK
 }
 
 unpack_iso() {
-    if [ "$(uname)" = "Darwin" ]; then
-        # macOS mount
-        # https://unix.stackexchange.com/questions/298685/can-a-mac-mount-a-debian-install-cd
-        hdiutil attach -nomount "ubuntu-${UBUNTU_RELEASE}.${PATCH_NUMBER}-server-amd64.iso"
-        # TODO: dynamically grab the disk that was attached
-        mount -t cd9660 /dev/disk2 "${ISO_MOUNT_DIR}"
-    else
-        # linux mount
-        # mount -o loop "ubuntu-${UBUNTU_RELEASE}.${PATCH_NUMBER}-server-amd64.iso" "${ISO_MOUNT_DIR}"
-        echo "LEL"
-    fi
-
+    # macOS mount
+    # https://unix.stackexchange.com/questions/298685/can-a-mac-mount-a-debian-install-cd
+    hdiutil attach -nomount "ubuntu-${UBUNTU_VERSION}.${PATCH_VERSION}-server-amd64.iso"
+    # TODO: dynamically grab the disk that was attached
+    mount -t cd9660 /dev/disk2 "${ISO_MOUNT_DIR}"
 
     # copy iso contents
-    sudo cp -R "${ISO_MOUNT_DIR}" "${CDIMAGE_DIR}"
+    rsync -av "${ISO_MOUNT_DIR}" "${CD_IMAGE_DIR}"
 
-    if [ "$(uname)" = "Darwin" ]; then
-        # macOS unmount
-        umount /dev/disk2
-        hdiutil detach /dev/disk2
-    else
-        # linux umount
-        # umount "${ISO_MOUNT_DIR}"
-        echo "LEL"
-    fi
+    unmount_iso
+}
+
+unmount_iso() {
+    umount /dev/disk2
+    hdiutil detach /dev/disk2
 }
 
 edit_bootloader() {
     # set timeout to 1s
-    sudo sed -i '' -e 's/^timeout 300/timeout 10/' cdimage/isolinux/isolinux.cfg
+    sed -i '' -e 's/^timeout 300/timeout 10/' ${CD_IMAGE_DIR}/${ISO_MOUNT_DIR}/isolinux/isolinux.cfg
     # set default to the LABEL nuc
-    sudo sed -i '' -e 's/^default.*/default nuc/' cdimage/isolinux/isolinux.cfg
+    sed -i '' -e 's/^default.*/default nuc/' ${CD_IMAGE_DIR}/${ISO_MOUNT_DIR}/isolinux/isolinux.cfg
     # add LABEL nuc
-    sudo tee -a cdimage/isolinux/isolinux.cfg <<EOF
+    tee -a ${CD_IMAGE_DIR}/${ISO_MOUNT_DIR}/isolinux/isolinux.cfg <<EOF
 LABEL nuc
   menu label ^NUC installation (preseed)
   kernel /install/vmlinuz
@@ -86,16 +89,16 @@ LABEL nuc
 EOF
 }
 copy_preseed() {
-    sudo cp ../preseed.cfg cdimage/preseed/nuc.seed
+    cp ../preseed.cfg ${CD_IMAGE_DIR}/${ISO_MOUNT_DIR}/preseed/nuc.seed
 }
 
 repack_iso() {
-    sudo mkisofs -r -V "Ubuntu for NUCs" \
+    mkisofs -r -V "Ubuntu ${UBUNTU_VERSION} preseed" \
             -cache-inodes \
             -J -l -b isolinux/isolinux.bin \
             -c isolinux/boot.cat -no-emul-boot \
             -boot-load-size 4 -boot-info-table \
-            -o "${CUSTOM_ISO_PATH}" "${CDIMAGE_DIR}"
+            -o "${CUSTOM_ISO_PATH}" "${CD_IMAGE_DIR}/${ISO_MOUNT_DIR}"
 }
 
 ########################
@@ -103,12 +106,13 @@ repack_iso() {
 # enter temp dir
 cd "${WORKDIR}"
 
+# download and verify
 download_iso
+download_checksums
 verify_files
 
 # following https://help.ubuntu.com/community/InstallCDCustomization
 unpack_iso
-
 copy_preseed
 edit_bootloader
 repack_iso
